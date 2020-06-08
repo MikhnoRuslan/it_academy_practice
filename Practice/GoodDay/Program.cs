a00171dc46fada399341679b33316f28a6a4c1ec
MikhnoRuslan <r.mikhno92@gmail.com>
2020-06-08 22:59:12 +0300

    done first taks. When i did i forgot "internal" for method class Greeting

--- Practice/GoodDay/Program.cs
+++ Practice/GoodDay/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace GoodDay
 {
@@ -6,7 +8,29 @@ namespace GoodDay
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DateTime dateTime = new DateTime();
+            Console.WriteLine(DateTime.Now.TimeOfDay);
+            Greeting greeting = new Greeting();
+            greeting.Print(dateTime);
+        }
+    }
+
+    class Greeting
+    {
+        private const byte Morning = 9;
+        private const byte Day = 12;
+        private const byte Evening = 15;
+        private const byte Night = 22;
+        internal void Print(DateTime dateTime)
+        {
+            if (dateTime.Hour > Morning && dateTime.Hour < 12)
+                Console.WriteLine($"Good morning, guys");
+            else if (dateTime.Hour > Day && dateTime.Hour < Evening)
+                Console.WriteLine($"Good day, guys");
+            else if (dateTime.Hour > Evening && dateTime.Hour < Night)
+                Console.WriteLine($"Good evening, guys");
+            else
+                Console.WriteLine($"Good night, guys");
         }
     }
 }
