f06262b639bb757f1b2b62600ae945d1c553ba6a
MikhnoRuslan <r.mikhno92@gmail.com>
2020-06-08 20:55:46 +0300

    done second task

---
+++ Practice/LettersCount/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LettersCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int sum = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                sum += 1;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
