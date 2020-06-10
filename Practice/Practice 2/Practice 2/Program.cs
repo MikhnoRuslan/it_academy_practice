c4b99b1676abf6222435dd9eca7de750613419b4
Ruslan Mikhno <r.mikhno92@gmail.com>
2020-06-10 21:07:08 +0300

    done 2 task

--- Practice/Practice 2/Practice 2/Program.cs
+++ Practice/Practice 2/Practice 2/Program.cs
@@ -6,12 +6,30 @@ namespace Practice_2
     {
         static void Main(string[] args)
         {
-
+            Tack2MixedArray();
         }
 
         static void Tack1EmptyArray()
         {
             int[] myArray = new int[] { }; 
+        }
+
+        static void Tack2MixedArray()
+        {
+            int firstValue = 32;
+            char secondValue = 'A';
+            string thirdValue = "Hello";
+            Object[] mixedArray = new Object[3];
+            mixedArray[0] = (int) firstValue;
+            mixedArray[1] = (char) secondValue;
+            mixedArray[2] = (string) thirdValue;
+            for (int i = 0; i < mixedArray.Length; i++)
+            {
+                Console.WriteLine(mixedArray[i] + " ");
+            }
+
+            int sumForFirstValue = 10;
+            string sumForThirdValue = ", guys!";
         }
     }
 }
