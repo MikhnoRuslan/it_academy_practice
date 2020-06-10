cb236734ae169f40296fe476e4a3119e65ebfe3e
Ruslan Mikhno <r.mikhno92@gmail.com>
2020-06-10 20:50:25 +0300

    first task completed

---
+++ Practice/Practice 2/Practice 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void Tack1EmptyArray()
+        {
+            int[] myArray = new int[] { }; 
+        }
+    }
+}
+  
