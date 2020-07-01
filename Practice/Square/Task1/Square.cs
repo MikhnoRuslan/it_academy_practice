0dd6f552c33114efd132f67bf9931ad466bef328
Ruslan Mikhno <r.mikhno92@gmail.com>
2020-07-01 22:35:31 +0300

    completed

---
+++ Practice/Square/Task1/Square.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task1
+{
+    class Square : Figure
+    {
+        public Square() {}
+
+        public Square(int firstSide, int secondSide)
+        {
+            FirstSide = firstSide;
+            SecondSide = secondSide;
+        }
+
+        internal int SetArea()
+        {
+            return FirstSide * SecondSide;
+        }
+
+        internal override void GetFigureData()
+        {
+            base.GetFigureData();
+            Console.WriteLine($" Square is {SetArea()}\n");
+        }
+    }
+}
---
+++ Practice/Square/Task1/Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task1
+{
+    class Triangle : Figure
+    {
+        public  Triangle() {}
+
+        public Triangle(int firstSide, int secondSide)
+        {
+            FirstSide = firstSide;
+            SecondSide = secondSide;
+        }
+
+        internal double SetArea()
+        {
+            return FirstSide * (double)SecondSide / 2;
+        }
+
+        internal override void GetFigureData()
+        {
+            base.GetFigureData();
+            Console.WriteLine($" Square is {SetArea()}\n");
+        }
+    }
+}
