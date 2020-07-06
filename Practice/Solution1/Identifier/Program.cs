bb905d4d5d2542c70d8c02e7a867d09a580e6f80
Ruslan Mikhno <r.mikhno92@gmail.com>
2020-07-06 20:14:07 +0300

    completed

---
+++ Practice/Solution1/Identifier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Identifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UniqueItem uniqueItem = new UniqueItem();
+        }
+    }
+}
---
+++ Practice/Solution1/Identifier/UniqueItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identifier
+{
+    class UniqueItem
+    {
+        static int _id = 0;
+
+        public static int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        static UniqueItem()
+        {
+            _id++;
+        }
+    }
+}
