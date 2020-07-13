4d88d62dc06ff13a46aa1b1b84ee8994010980cd
Ruslan Mikhno <r.mikhno92@gmail.com>
2020-07-13 23:25:32 +0300

    task 3 is completed

---
+++ Practice/Practice14/Task3/Neighbor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task3
+{
+    class Neighbor
+    {
+        public string FullName { get; set; }
+        public int FlatNumber { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Neighbor(string fullName, int flatNumber, string phoneNumber)
+        {
+            FullName = fullName;
+            FlatNumber = flatNumber;
+            PhoneNumber = phoneNumber;
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine($"Full name: {FullName};\nFlat number: {FlatNumber};\nPhone number: {PhoneNumber}.");
+        }
+    }
+}
--- Practice/Practice14/Task3/Program.cs
+++ Practice/Practice14/Task3/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Task3
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            ConsoleKeyInfo key;
+            List<Neighbor> neighbors = new List<Neighbor>();
+
+            do
+            {
+                neighbors.Add(new Neighbor(AddFullName(), AddFlatNumber(), AddPhoneNumber()));
+                Console.WriteLine($"Do you want to add more (Y/N)?");
+                key = Console.ReadKey();
+                Console.WriteLine();
+            } while (key.Key != ConsoleKey.N);
+
+            var numFlat = AddFlatNumber();
+            
+            for (int i = 0; i < neighbors.Count; i++)
+            {
+                if (neighbors[i].FlatNumber.Equals(numFlat))
+                    neighbors[i].ShowData();
+            }
+        }
+
+        static string AddFullName()
+        {
+            Console.WriteLine($"Enter full name:");
+            return Console.ReadLine();
+        }
+
+        static int AddFlatNumber()
+        {
+            Console.WriteLine($"Enter flat number");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static string AddPhoneNumber()
+        {
+            Console.WriteLine($"Enter phone number:");
+            return Console.ReadLine();
         }
     }
 }
